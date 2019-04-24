4ba9bc50e54087bfe8e8b8d20768d9378a37220e
myadomin <adomin@163.com>
2019-04-24 18:38:12 +0800

    样式调节

--- pages/articleContent/articleContent.js
+++ pages/articleContent/articleContent.js
@@ -1,55 +1,48 @@
-// pages/articleContent/articleContent.js
 const ajax = require('../../utils/ajax.js')
 const urls = require('../../config/urls.js')
 const WxParse = require('../../libs/wxParse/wxParse.js');
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     startRep: '',
     endRep: '',
     endParse: '',
-    nodes: '',
+    title: '',
+    author: '',
+    html: '',
     vid: ''
   },
 
-  // 解析html
-  parseContent (html) {
-    const aa = html.replace(/section/g, 'div')
+  // 处理html字符串
+  parseHtml(content) {
+    const html = content.replace(/section/g, 'div')
     .replace(/<(img|table) ([\s\S]*?)(\/>|>)/g, '<$1 $2 style="width: 100%!important" />')
     .replace(/<wxs ([\s\S]*?) <\/wxs>/g, '')
-      // .split(/<iframe allowfullscreen[\s\S]*?<\/iframe>/g)
-    // console.log(aa)
-    return aa
+    return html
   },
 
   // 获取vid
   getQueryString (url, query) {
     const reg = new RegExp('(^|&)' + query + '=([^&]*)(&|$)', 'i');
     const r = url.substr(1).match(reg);
-    if (r != null) {
-      return unescape(r[2]);
-    }
-    return null;
+    return r ? unescape(r[2]) : null;
   },
 
   // 获取文章详情
   getArticleContentById (id) {
     this.setData({ startRep: new Date().getTime() })
-    wx.showLoading({
-      title: '加载中'
-    })
+    wx.showLoading({ title: '加载中' })
     ajax.get(urls.getArticleContentById(id)).then(res => {
       this.setData({ endRep: new Date().getTime() })
-      // const content = res.data.content.content
-      // WxParse.wxParse('article', 'html', content, this, 0);
-      const content = this.parseContent(res.data.content.content)
-      this.setData({ nodes: content })
-      this.setData({ vid: this.getQueryString(res.data.content.video_url, 'vid') })
-      this.setData({ endParse: new Date().getTime() })
+      const { title, author, content, video_url } = res.data.content
+      this.setData({
+        title,
+        author,
+        html: this.parseHtml(content),
+        vid: this.getQueryString(video_url, 'vid'),
+        endParse: new Date().getTime()
+      })
       wx.hideLoading()
     })
   },
